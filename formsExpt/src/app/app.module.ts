5601a787448c1871599ec2925be06a5759e71c7c
Tanuj Chakraborty <tans.c2@gmail.com>
2020-09-22 22:22:39 +0530

    added customer details form

--- formsExpt/src/app/app.module.ts
+++ formsExpt/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    CustomerDetailsComponent
   ],
   imports: [
     BrowserModule,
